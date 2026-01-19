103fd639d65d4c5f3cfbd1a3620571f40e34b03c
mpalmer79 <mpalm2025@gmail.com>
2026-01-19 14:08:04 -0500

    Create index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Header } from './Header';
+export { default as SetupScreen } from './SetupScreen';
+export { default as TrainingScreen } from './TrainingScreen';
+export { default as FeedbackScreen } from './FeedbackScreen';
