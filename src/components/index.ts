378f8cf2c1df4f839575fd8fbd400285e7fd5e9e
mpalmer79 <mpalm2025@gmail.com>
2026-01-20 17:11:35 -0500

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,5 @@ export { default as SetupScreen } from './SetupScreen';
 export { default as TrainingScreen } from './TrainingScreen';
 export { default as MobileTrainingScreen } from './MobileTrainingScreen';
 export { default as FeedbackScreen } from './FeedbackScreen';
+export { default as ProgressDashboard } from './ProgressDashboard';
+export { default as SessionDetailModal } from './SessionDetailModal';
