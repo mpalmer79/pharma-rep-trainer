245dc109196d225873cf28a083ff3e23cc048f05
mpalmer79 <mpalm2025@gmail.com>
2026-01-20 17:00:35 -0500

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Header } from './Header';
 export { default as SetupScreen } from './SetupScreen';
 export { default as TrainingScreen } from './TrainingScreen';
+export { default as MobileTrainingScreen } from './MobileTrainingScreen';
 export { default as FeedbackScreen } from './FeedbackScreen';
