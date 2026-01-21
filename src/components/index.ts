72aebcde262a02017101870793e034ffc3a62146
mpalmer79 <mpalm2025@gmail.com>
2026-01-21 15:26:25 -0500

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,14 +1,12 @@
+export { default as FeedbackScreen } from './FeedbackScreen';
+export { default as FeedbackStage } from './FeedbackStage';
 export { default as Header } from './Header';
+export { default as ManagerDashboard } from './ManagerDashboard';
+export { default as MobileTrainingScreen } from './MobileTrainingScreen';
+export { default as ProgressDashboard } from './ProgressDashboard';
+export { default as Providers } from './Providers';
+export { default as SessionDetailModal } from './SessionDetailModal';
 export { default as SetupScreen } from './SetupScreen';
 export { default as TrainingScreen } from './TrainingScreen';
-export { default as MobileTrainingScreen } from './MobileTrainingScreen';
-export { default as FeedbackScreen } from './FeedbackScreen';
-export { default as ProgressDashboard } from './ProgressDashboard';
-export { default as SessionDetailModal } from './SessionDetailModal';
-export { default as FeedbackStage } from './FeedbackStage';
-
-// UI Components
-export * from './ui';
-
-// Landing Page Components
-export * from './landing';
+export { default as UserMenu } from './UserMenu';
+export { default as ObjectionBank } from './ObjectionBank';
