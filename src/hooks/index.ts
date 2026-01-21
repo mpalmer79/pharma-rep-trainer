26a8fabad8320658f85088c8c9faee6f4c463c8f
mpalmer79 <mpalm2025@gmail.com>
2026-01-21 12:36:31 -0500

    Update index.ts

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,6 @@
 export { default as useSessionHistory } from './useSessionHistory';
 export type { ProgressStats, SessionHistoryData } from './useSessionHistory';
 export { useScrollAnimation } from './useScrollAnimation';
+export { useHybridSession } from './useHybridSession';
+export type { SessionRecord, ProgressStats as HybridProgressStats } from './useHybridSession';
+export { useSupabaseSession } from './useSupabaseSession';
